d9e185087bcbdc384dce3ebbc3dbee665b56e7cf
Trust <fogrady23@galwayetss.ie>
2026-01-10 16:02:24 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
+// This is a simple plan for your server
+const express = require('express');
+const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Trust Games Server is Awake!');
+});
+
+// This part handles the 1 Trillion Credit limit
+app.post('/give-credits', (req, res) => {
+    const amount = req.body.amount;
+    if (amount > 1000000000000) {
+        res.send('Error: Too many credits!');
+    } else {
+        res.send('Credits granted!');
+    }
+});
+
+console.log("Server logic ready.");
